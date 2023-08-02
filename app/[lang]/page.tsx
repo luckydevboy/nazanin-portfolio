566927211817e8223715198d5260a522d7e73296
Mohammad Reza Ghasemi <mrghasemi1992@gmail.com>
2023-08-02 21:11:16 +0330

    chore: add google analytics

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -4,26 +4,31 @@ import Introduction from "../../components/Introduction";
 import Educations from "@/components/Educations";
 import Experiences from "@/components/Experiences";
 import Skills from "@/components/Skills";
+import Script from "next/script";
 
 export default async function Home({
   params: { lang },
 }: {
   params: { lang: Locale };
 }) {
-  const dictionary =
-    await getDictionary(lang);
+  const dictionary = await getDictionary(lang);
 
   return (
     <main>
-      <Introduction
-        dictionary={dictionary}
-      />
-      <Educations
-        dictionary={dictionary}
-      />
-      <Experiences
-        dictionary={dictionary}
-      />
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-9YBDWNZVNW" />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', 'GA_MEASUREMENT_ID');
+        `}
+      </Script>
+
+      <Introduction dictionary={dictionary} />
+      <Educations dictionary={dictionary} />
+      <Experiences dictionary={dictionary} />
       <Skills dictionary={dictionary} />
     </main>
   );
