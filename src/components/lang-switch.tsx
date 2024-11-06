131cd3b79d682145fc2b90b21bc443d20777b825
Mohammad Reza Ghasemi <mrghasemi1992@gmail.com>
2024-11-06 07:59:34 +0330

    feat: animate lang-switch component

--- src/components/lang-switch.tsx
+++ src/components/lang-switch.tsx
@@ -6,8 +6,9 @@ import * as React from "react";
 import { useRouter } from "next/navigation";
 import { useRef, useState } from "react";
 import { useClickAway } from "react-use";
+import { motion, AnimatePresence } from "framer-motion";
 
-const LangSwitch = () => {
+export default function LangSwitch() {
   const [showActions, setShowActions] = useState(false);
   const router = useRouter();
   const ref = useRef(null);
@@ -30,27 +31,34 @@ const LangSwitch = () => {
 
   return (
     <div ref={ref}>
-      <div
+      <motion.div
         className="fixed cursor-pointer right-4 bottom-4 xl:right-8 xl:bottom-8 bg-white rounded-full p-2 shadow-primary shadow-lg"
         onClick={() => setShowActions(!showActions)}
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.9 }}
       >
         <Image src={globe} width={24} height={24} alt="Globe" />
-      </div>
-      {showActions && (
-        <div className="fixed flex flex-col gap-y-4 bottom-16 right-4 xl:right-8 xl:bottom-20 bg-white rounded-full p-3 shadow-primary shadow-lg">
-          {actions.map((action) => (
-            <div
-              className="cursor-pointer hover:font-bold"
-              onClick={action.onClick}
-              key={action.name}
-            >
-              {action.icon}
-            </div>
-          ))}
-        </div>
-      )}
+      </motion.div>
+      <AnimatePresence>
+        {showActions && (
+          <motion.div
+            className="fixed flex flex-col gap-y-4 bottom-16 right-4 xl:right-8 xl:bottom-20 bg-white rounded-full p-3 shadow-primary shadow-lg"
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: 20 }}
+          >
+            {actions.map((action) => (
+              <div
+                className="cursor-pointer hover:font-bold"
+                onClick={action.onClick}
+                key={action.name}
+              >
+                {action.icon}
+              </div>
+            ))}
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
-};
-
-export default LangSwitch;
+}
